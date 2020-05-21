bc601a500473b566c3341dfc5f0d24d5906bcaac
xinshangshangxin <xinshangshangxin@gmail.com>
2020-05-21 13:16:32 +0800

    fix: restart snet failed

--- main/src/entry/power-monitor.ts
+++ main/src/entry/power-monitor.ts
@@ -1,13 +1,15 @@
 import { powerMonitor } from 'electron';
 import { EventEmitter } from 'events';
 import { from, fromEvent, merge, of } from 'rxjs';
-import { catchError, debounceTime, switchMap } from 'rxjs/operators';
+import { catchError, switchMap, throttleTime } from 'rxjs/operators';
 
 import { instance } from './instance';
 
 async function restartSnet() {
-  if (instance.snet?.isRunning) {
-    await instance.snet.stop({ persistStatus: false, notify: false, cleanPf: true });
+  const isRunning = instance.snet?.isRunning;
+
+  await instance.snet.stop({ persistStatus: false, notify: false, cleanPf: true });
+  if (isRunning) {
     await instance.snet.start({ notify: false });
   }
 }
@@ -16,7 +18,7 @@ function onResume() {
   const pm = powerMonitor as EventEmitter;
 
   return merge(fromEvent(pm, 'resume'), fromEvent(pm, 'unlock-screen')).pipe(
-    debounceTime(200),
+    throttleTime(1000),
     switchMap(() => {
       return from(restartSnet());
     }),
